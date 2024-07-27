d80edf1356971f8d09a12fd51fdfa17b128f2147
SurendraPatidar1 <131394762+SurendraPatidar1@users.noreply.github.com>
2024-07-27 18:12:21 +0000

    02_objects.js

--- 02_Intermediate/02_object.js
+++ 02_Intermediate/02_object.js
@@ -1,5 +1,5 @@
 
-//Object Literals
+// *************       Object Literals      ****************
 const mySym = Symbol("mykey");
 const user = {
     name: "Surya",
@@ -11,8 +11,23 @@ const user = {
     isLoggedIn: false,
     lastLoginDays:["Monday" , "Tuesday"]
 }
-console.log(user); // Whole object
-console.log(user.email); //  surendrapatidar001@gmail.com 
-console.log(user["email"]); // surendrapatidar001@gmail.com 
-console.log(user["full name"]); //Surendra Patidar
-console.log(typeof user.mySym);
+// console.log(user); // Whole object
+// console.log(user.email); //  surendrapatidar001@gmail.com 
+// console.log(user["email"]); // surendrapatidar001@gmail.com 
+// console.log(user["full name"]); //Surendra Patidar
+// console.log(user[mySym]); // it is used for access the Symbol object.
+
+
+user.email = "surya@chatgpt.com";
+// Object.freeze(user); // if for ex. user email is freeze so we don't change in future.
+user.email = "dev@ai.com"; // the value will not change because we use freeze object for freeze the object or not change further
+console.log(user);
+
+user.greeting = function(){
+    console.log("Hello JavaScript Learners");
+}
+user.greetingTwo = function(){
+    console.log(`Hello JavaScript Learners,${this.name}`);
+}
+console.log(user.greeting());
+console.log(user.greetingTwo());
