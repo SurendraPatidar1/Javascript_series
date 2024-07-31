761d58790dfb60b95084403406aa680f7a85eeab
SurendraPatidar1 <131394762+SurendraPatidar1@users.noreply.github.com>
2024-07-31 12:35:46 +0000

    01_basic.js

---
+++ 03_ControlFlow/01_basic.js
@@ -0,0 +1,105 @@
+// const name = true;
+// if(name){
+//     console.log("surya");
+// }
+
+// if(2 == "2"){
+//     console.log("Executed");
+// }
+
+// const temp = 10;
+// if(temp == 50)
+// {
+//     console.log("less than 10");
+// }
+// else{
+//     console.log("Temp is greater than 10");
+// }
+ // < , > , <= , >= , == , != , === , !== 
+
+// const score = 200;
+// if(score > 100)
+// {
+//     var power = "fly";
+//     console.log(`User Power : ${power}`); // User Power : fly
+// }
+//console.log(`User Power : ${power}`); // User Power : fly , because we use var 
+// so the scope of var is entire program that's why we are not using var .
+
+const balance = 1000;
+// if(balance > 500) console.log("Yes,balance is greater than 500");
+
+
+// ************* if-else / else-if ********************
+// if(balance < 500) 
+// {
+//     console.log("It is less than 500");
+// }
+// else if(balance < 750)
+// {
+//     console.log("It is less than 750");
+// }
+// else if(balance < 900)
+// {
+//     console.log("It is less than 900");
+// }
+// else
+// {
+//     console.log("It is Greater than 900");
+// }
+ 
+// ****************** or and operator && , ||  ******************
+// const userLoggedIn = true;
+// const debitCard = true;
+// const loggedInFromGoogle = false;
+// const loggedInFromEmail = true;
+// if(userLoggedIn && debitCard)
+// {
+//     console.log("Allow to buy Car");
+// }
+// if(loggedInFromGoogle || loggedInFromEmail)
+// {
+//     console.log("User logged In");
+// }
+
+// *************** syntax of using switch statement .******************
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+// const month = 3;
+// switch(month){
+//     case 1:
+//         console.log("january");
+//         break;
+//     case 2:
+//         console.log("Febuary");
+//         break;
+//     case 3:
+//         console.log("March");
+//         break;
+//     default:
+//         console.log("Another Month");
+//         break;
+// }
+
+// const month = "March";
+// switch(month){
+//     case "jan":
+//         console.log("january");
+//         break;
+//     case "Feb":
+//         console.log("Febuary");
+//         break;
+//     case "March":
+//         console.log("March");
+//         break;
+//     default:
+//         console.log("Another Month");
+//         break;
+// }
