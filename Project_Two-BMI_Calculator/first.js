f58fc40223a6b88995cf2a458881f255cf989ddc
SurendraPatidar1 <131394762+SurendraPatidar1@users.noreply.github.com>
2024-08-04 04:57:09 +0000

    first.js

--- Project_Two-BMI_Calculator/first.js
+++ Project_Two-BMI_Calculator/first.js
@@ -9,7 +9,7 @@ form.addEventListener('submit' , function(e){
     const results = document.querySelector('#results');
 
     if(height === '' || height < 0 || isNaN(height)){
-        results.innerHTML = `PLease give valid height${height}`;
+        results.innerHTML = `Please give valid height , ${height}`;
     }else if(weight === '' || weight < 0 || isNaN(weight)){
         results.innerHTML = `Please give valid weight , ${weight}`;
     }else{
