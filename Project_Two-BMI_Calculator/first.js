867a76d047265afbcdc97cde98559a452b71ea83
SurendraPatidar1 <131394762+SurendraPatidar1@users.noreply.github.com>
2024-08-04 04:55:05 +0000

    first.html

--- Project_Two-BMI_Calculator/first.js
+++ Project_Two-BMI_Calculator/first.js
@@ -1,1 +1,20 @@
 const form = document.querySelector('form');
+// this case will give you empty value after reload 
+// const height = parseInt(document.querySelector('#height').value);
+form.addEventListener('submit' , function(e){
+    e.preventDefault();
+
+    const height = parseInt(document.querySelector('#height').value);
+    const weight = parseInt(document.querySelector('#weight').value);
+    const results = document.querySelector('#results');
+
+    if(height === '' || height < 0 || isNaN(height)){
+        results.innerHTML = `PLease give valid height${height}`;
+    }else if(weight === '' || weight < 0 || isNaN(weight)){
+        results.innerHTML = `PLease give valid weight${weight}`;
+    }else{
+        const bmi = (weight / ((height*height)/10000)).toFixed(2);
+
+        results.innerHTML = `<span>${bmi}</span>`;
+    }
+});
