feeb596901b58c35b85f9f85a87f5b105dd0b7e8
Surendra Patidar <131394762+SurendraPatidar1@users.noreply.github.com>
2024-08-11 09:43:15 +0000

    properties_get_set.js

--- 05_Oops_Concepts/properties_get_set.js
+++ 05_Oops_Concepts/properties_get_set.js
@@ -1,23 +1,39 @@
-function User(email, password){
-    this._email = email;
-    this._password = password;
+// function User(email, password){
+//     this._email = email;
+//     this._password = password;
 
-    Object.defineProperty(this,'email',{
-        get: function(){
-            return this._email.toUpperCase();
-        },
-        set: function(value){
-            this._email = value;
-        }
-    })
-    Object.defineProperty(this,'password',{
-        get: function(){
-            return this._password.toUpperCase();
-        },
-        set: function(value){
-            this._password = value;
-        }
-    })
+//     Object.defineProperty(this,'email',{
+//         get: function(){
+//             return this._email.toUpperCase();
+//         },
+//         set: function(value){
+//             this._email = value;
+//         }
+//     })
+//     Object.defineProperty(this,'password',{
+//         get: function(){
+//             return this._password.toUpperCase();
+//         },
+//         set: function(value){
+//             this._password = value;
+//         }
+//     })
+// }
+// const chai = new User("chai@chai.com","chai");
+// console.log(chai.email);
+
+/// This syntax is rarely used but in past time it is used so don't get confused..
+const User = {
+    _email: 'Sur@s.com',
+    _password: "abc",
+
+    get email(){
+        return this._email.toUpperCase();
+    },
+    
+    set email(value){
+         this._email = value;
+    }
 }
-const chai = new User("chai@chai.com","chai");
-console.log(chai.email);
+const tea = Object.create(User);
+console.log(tea.email);
